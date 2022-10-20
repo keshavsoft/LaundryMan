da557b97d64f080dd7832196a1a69ce6446500c3
Tulasi1920 <100200964+Tulasi1920@users.noreply.github.com>
2022-10-20 18:05:59 +0530

    Home to Masters

--- resources/Laundry/Masters/Customers/Js/CustomersToDOM.js
+++ resources/Laundry/Masters/Customers/Js/CustomersToDOM.js
@@ -11,7 +11,7 @@ let CustomersHeaderFunc = async () => {
 };
 
 let LocalAddListeners = () => {
-    let jVarLocalHomeId = document.getElementById("HomeId");
+    let jVarLocalHomeId = document.getElementById("MastersHeaderId");
     let jVarLocalCustomersInsertHeaderId = document.getElementById("CustomersInsertHeaderId");
     let jVarLocalCustomersShowHeaderId = document.getElementById("CustomersShowHeaderId");
 
