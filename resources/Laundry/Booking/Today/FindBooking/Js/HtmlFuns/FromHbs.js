b9c5dca3b75dd26b1762a81ae69b5cecf057508c
Tulasi1920 <100200964+Tulasi1920@users.noreply.github.com>
2022-10-26 18:35:05 +0530

    Hbs For Find Booking

--- resources/Laundry/Booking/Today/FindBooking/Js/HtmlFuns/FromHbs.js
+++ resources/Laundry/Booking/Today/FindBooking/Js/HtmlFuns/FromHbs.js
@@ -1,8 +1,22 @@
-let FindBookingShowtHtmlFunc = async () => {
-    let jVarLocalFetchUrl = "Laundry/Booking/Today/FindBooking/Html/Hbs/FindBookingShow.html";
+let CustomerDetailstHtmlFunc = async () => {
+    let jVarLocalFetchUrl = "Laundry/Booking/Today/FindBooking/Html/Hbs/CustomerDetails.html";
     let response = await fetch(jVarLocalFetchUrl);
     let data = await response.text();
     return await data;
 };
 
-export { FindBookingShowtHtmlFunc }
+let GarmentDetailsHtmlFunc = async () => {
+    let jVarLocalFetchUrl = "Laundry/Booking/Today/FindBooking/Html/Hbs/GarmentDetails.html";
+    let response = await fetch(jVarLocalFetchUrl);
+    let data = await response.text();
+    return await data;
+};
+
+let QrCodeArrayHtmlFunc = async () => {
+    let jVarLocalFetchUrl = "Laundry/Booking/Today/FindBooking/Html/Hbs/QrcodeArray.html";
+    let response = await fetch(jVarLocalFetchUrl);
+    let data = await response.text();
+    return await data;
+};
+
+export { CustomerDetailstHtmlFunc,GarmentDetailsHtmlFunc,QrCodeArrayHtmlFunc };
